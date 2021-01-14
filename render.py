17704b80c28c230f489fa39dc6855226bd283bbd
mraad <mraad23@gmail.com>
2021-01-14 13:49:24 -0500

    Updated rendering code

--- render.py
+++ render.py
@@ -1,25 +1,38 @@
+import click
 import gym
-import cv2
-from gym import wrappers
+import torch
+from gym.wrappers.monitoring.video_recorder import VideoRecorder
+
+from rl.model import MLP
+
+
+@click.command()
+@click.option('--env_name', type=str, default="LunarLander-v2", show_default=True, help='Environment name.')
+@click.option('--pth_path', type=str, default="2021_01_13_06_17.pth", help='Path of pytorch weight file.')
+@click.option('--mp4_path', type=str, default="lunarlander.mp4", help='Path of MP4 file.')
+def main(env_name: str,
+         pth_path: str,
+         mp4_path: str
+         ) -> None:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    with gym.make(env_name) as env:
+        n_states = env.observation_space.shape[0]
+        n_actions = env.action_space.n
+        model = MLP(n_states, n_actions)
+        model.load_state_dict(torch.load(pth_path, map_location=device))
+        model.eval()
+        rec = VideoRecorder(env, mp4_path)
+        state = env.reset()
+        done = False
+        while not done:
+            env.render()
+            rec.capture_frame()
+            state = torch.from_numpy(state).to(device)
+            action = model(state).argmax().item()
+            # action = env.action_space.sample()
+            state, reward, done, _ = env.step(action)
+        rec.close()
+
 
 if __name__ == '__main__':
-    frames = []
-    env = gym.make("LunarLander-v2")
-    # env = wrappers.Monitor(env, "./gym-results", force=True)
-    state = env.reset()
-    done = False
-    while not done:
-        # env.render()
-        frames.append(env.render(mode='rgb_array'))
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-    env.close()
-
-    size = (96, 96)
-
-    out = cv2.VideoWriter('LunarLander.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
-
-    for frame in frames:
-        rgb_img = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        out.write(rgb_img)
-    out.release()
+    main()
--- rl/utils.py
+++ rl/utils.py
@@ -10,13 +10,13 @@ class Utils:
             steps: int,
             log_start: float = -2.0) -> np.array:
         assert steps > 2, "dec_schedule steps should be greater than 2"
-        decay_steps = max(2, int(steps * ratio))
-        values = np.logspace(log_start, 0, decay_steps)[::-1]
+        log_steps = max(2, int(steps * ratio))
+        values = np.logspace(log_start, 0, log_steps)[::-1]
         v_min = values.min()
         v_max = values.max()
         values = (values - v_min) / (v_max - v_min)
         values = final + (start - final) * values
-        pad = steps - decay_steps
+        pad = steps - log_steps
         values = np.pad(values, (0, pad), 'edge')
         return values
 
@@ -28,12 +28,12 @@ class Utils:
             steps: int,
             log_start: float = -2.0) -> np.array:
         assert steps > 2, "inc_schedule steps should be greater than 2"
-        decay_steps = max(2, int(steps * ratio))
-        values = np.logspace(log_start, 0, decay_steps)
+        log_steps = max(2, int(steps * ratio))
+        values = np.logspace(log_start, 0, log_steps)
         v_min = values.min()
         v_max = values.max()
         values = (values - v_min) / (v_max - v_min)
         values = start + (final - start) * values
-        pad = steps - decay_steps
+        pad = steps - log_steps
         values = np.pad(values, (0, pad), 'edge')
         return values
